8ede0e50c5740c2133f71b5cace001d9e366a7bc
a5ea5em5int <44253828+a5ea5em5int@users.noreply.github.com>
2020-01-30 14:19:35 +0630

    merged with develoop

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,70 @@
+package com.napier.sem;
+
+public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    private String capital;
+    private int population;
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", region='" + region + '\'' +
+                ", capital='" + capital + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
